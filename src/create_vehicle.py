40644531980520e68d860c1f3cd49afbe6ff934d
Erick Ahmed <engineering@erickahmed.com>
2025-02-01 01:02:50 +0100

    Fix duplicate error

--- src/create_vehicle.py
+++ src/create_vehicle.py
@@ -2,7 +2,7 @@ import sqlite3
 import os
 import argparse
 
-def create_vehicle_database(vehicle_name, vehicle_version, mass, wheelbase, inertia,
+def create_vehicle_database(vehicle_name, vehicle_version, mass, halfbase_F, halfbase_R,
                            steering_ratio):
 
     folder_path = os.path.join('../db/vehicles/', vehicle_name, vehicle_version)
@@ -19,8 +19,8 @@ def create_vehicle_database(vehicle_name, vehicle_version, mass, wheelbase, iner
     CREATE TABLE IF NOT EXISTS vehicle_specs (
         name TEXT PRIMARY KEY,
         mass REAL,
-        wheelbase REAL,
-        inertia REAL
+        halfbase_F REAL,
+        halfbase_R REAL,
     )
     ''')
 
@@ -37,7 +37,7 @@ def create_vehicle_database(vehicle_name, vehicle_version, mass, wheelbase, iner
     cursor.execute('''
     INSERT OR REPLACE INTO vehicle_specs (name, mass, wheelbase, inertia)
     VALUES (?, ?, ?, ?)
-    ''', (vehicle_name, mass, wheelbase, inertia))
+    ''', (vehicle_name, mass, halfbase_F, halfbase_R))
 
     # Insert tire specifications into the database
     cursor.execute('''
@@ -49,8 +49,8 @@ def create_vehicle_database(vehicle_name, vehicle_version, mass, wheelbase, iner
     ''', (
         vehicle_name,
         mass,
-        wheelbase,
-        inertia,
+        halfbase_F,
+        halfbase_R,
         vehicle_name,
         steering_ratio
     ))
@@ -77,8 +77,8 @@ if __name__ == "__main__":
         vehicle_name=args.vehicle_name,
         vehicle_version=args.vehicle_version,
         mass=args.mass,
-        wheelbase=args.wheelbase,
-        inertia=args.inertia,
+        halfbase_F=args.halfbase_F,
+        halfbase_R=args.halfbase_R,
         steering_ratio=args.steering_ratio,
     )
 
@@ -90,8 +90,8 @@ if __name__ == "__main__":
     parser.add_argument('vehicle_name', type=str, help='Name of the vehicle')
     parser.add_argument('vehicle_version', type=str, help='Version of the vehicle')
     parser.add_argument('mass', type=float, help='Mass of the vehicle')
-    parser.add_argument('wheelbase', type=float, help='Wheelbase of the vehicle')
-    parser.add_argument('inertia', type=float, help='Inertia of the vehicle')
+    parser.add_argument('halfbase_F', type=float, help='Half wheelbase rear')
+    parser.add_argument('halfbase_R', type=float, help='Half wheelbase front')
     parser.add_argument('steering_ratio', type=float, help='Steering to wheel turning ratio')
 
     args = parser.parse_args()
@@ -99,7 +99,7 @@ if __name__ == "__main__":
         vehicle_name=args.vehicle_name,
         vehicle_version=args.vehicle_version,
         mass=args.mass,
-        wheelbase=args.wheelbase,
-        inertia=args.inertia,
+        halfbase_F=args.halfbase_F,
+        halfbase_R=args.halfbase_R,
         steering_ratio=args.steering_ratio,
     )
