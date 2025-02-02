0d206985984c142a41dc86e849ce9da882b95b82
Erick Ahmed <engineering@erickahmed.com>
2025-02-02 21:29:09 +0100

    Substitute vehicle parameters loader with vehicle_database.load()

--- src/vehicle_model.py
+++ src/vehicle_model.py
@@ -34,7 +34,7 @@ class Steering:
 
 class Vehicle:
     def __init__(self, vehicle_name, vehicle_version,
-                x, y, z, roll, pitch, yaw,  #TODO: when rewrite in rust use array
+                x, y, z, roll, pitch, yaw,  #NOTE: when rewrite in rust use array
                 velocity,
                 steering: Steering):
         self.x = x
@@ -46,44 +46,18 @@ class Vehicle:
         self.velocity = velocity
         self.wheel_angle_F = steering
 
-        self.load_vehicle_parameters(vehicle_name, vehicle_version)
-
-    # TODO: use vehicle_database.py
-    def load_vehicle_parameters(self, vehicle_name, vehicle_version):
-        """Load vehicle parameters from the database"""
-        db_path = os.path.join('../db/vehicles/', vehicle_name, vehicle_version, 'parameters.db')
-
-        if not os.path.exists(db_path):
-            raise FileNotFoundError(f"Database for vehicle '{vehicle_name}' not found at {db_path}.")
-
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-
-        cursor.execute('''
-            SELECT mass, COG_x, COG_y, COG_z,
-            FROM vehicle_specs WHERE name = ?
-        ''', (vehicle_name,))
-
-        row = cursor.fetchone()
-        if row:
-            self.mass, self.COG_x, self.COG_y, self.COG_z = row
-        else:
-            raise ValueError(f"Vehicle '{vehicle_name}' not found in the database.")
-
-        conn.close()
+        # Load vehicle parameters from database
+        mass      = load_db(vehicle_name, vehicle_version, 'mass')
+        wheelbase = load_db(vehicle_name, vehicle_version, 'wheelbase')
+        cog_x     = load_db(vehicle_name, vehicle_version, 'cog_x')
+        cog_y     = load_db(vehicle_name, vehicle_version, 'cog_y')
+        cog_z     = load_db(vehicle_name, vehicle_version, 'cog_z')
 
     def single_track_model(self, velocity, wheel_angle):
-        #this is just a very rude approximation!
-        #longitudinal_velocity = velocity * math.sin(wheel_angle)
-        #lateral_velocity = velocity * math.cos(wheel_angle)
-
-        #slip_angle = math.atan(lateral_velocity / longitudinal_velocity)
-        #ackermann_angle = math.atan((wheelbase * self.yaw) / longitudinal_velocity)
-
-        return 0
+        pass
 
     def update(self, dt):
-        #longitudinal_velocity, lateral_velocity, slip_angle, ackermann_angle = self.single_track_model(self.velocity, self.wheel_angle_F)
+        #something =  self.single_track_model(self.velocity, self.wheel_angle_F)
 
         #self.x += longitudinal_velocity * dt
         #self.y += lateral_velocity * dt
